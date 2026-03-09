7f3c51403298619ee7f17c65f8bb882291128071
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-03-09 16:38:36 -0300

    refactor: changed routes for more organization

--- src/routes/auth-v1.routes.ts
+++ src/routes/auth-v1.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { loginController } from "../http/auth/login.http";
+import { createUserController } from "../http/users/createUser.http";
 
 const authRoutes = Router();
 
-authRoutes.post("/", loginController);
+authRoutes.post("/login", loginController);
+authRoutes.post("/register", createUserController);
 
 export default authRoutes;
