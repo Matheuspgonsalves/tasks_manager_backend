c62ee0bc3bd4a71f4b61173b41c3673185a3a363
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-03-11 20:15:49 -0300

    config: turing cors on

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import cors from "cors";
 import express from "express";
 import "dotenv/config";
 import routes from "./routes/index.routes";
 
 const app = express();
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL].filter(
+  (origin): origin is string => Boolean(origin)
+);
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
+  })
+);
 app.use(express.json());
 app.use(routes);
 app.get("/", (req: any, res: any) => {
