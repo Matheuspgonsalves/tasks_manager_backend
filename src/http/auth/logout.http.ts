e4a02718cbdced8bbfd2b9ad919ab7507669ce73
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-03-15 17:35:29 -0300

    feat: created logout endpoint

---
+++ src/http/auth/logout.http.ts
@@ -0,0 +1,19 @@
+import { Response } from "express"
+import { clearAccessTokenCookie } from "../../utils/cookies.util";
+import { AuthRequest } from "../../interfaces/AuthRequest.interface";
+
+export const logoutController = async (req: AuthRequest, res: Response) => {
+  try {
+    clearAccessTokenCookie(res);
+
+    return res.status(200).send({
+        success: true,
+        message: "Logged out successfully",
+    });
+  } catch (error: any) {
+    return res.status(500).send({
+      success: false,
+      message: "Logout failed",
+    });
+  }
+}
--- src/routes/auth-v1.routes.ts
+++ src/routes/auth-v1.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { loginController } from "../http/auth/login.http";
 import { createUserController } from "../http/users/createUser.http";
+import { logoutController } from "../http/auth/logout.http";
+import authMiddleware from "../middlewares/auth.middleware";
 
 const authRoutes = Router();
 
 authRoutes.post("/login", loginController);
 authRoutes.post("/register", createUserController);
+authRoutes.post("/logout", authMiddleware.checkToken, logoutController);
 
 export default authRoutes;
