a71c4fc4a04c7d3e29901c2583cb2a1fb2c959f0
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-03-15 18:24:22 -0300

    fix: handler health error

--- src/http/health/health.http.ts
+++ src/http/health/health.http.ts
@@ -1,6 +1,6 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 
-export const healthCheckerController = async (res: Response) => {
+export const healthCheckerController = async (_req: Request, res: Response) => {
   return res.status(200).send({
     success: true,
     message: "Server running",
