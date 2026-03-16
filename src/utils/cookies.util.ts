b90b27c6e85275c28840b04c536aa076d2b74fb6
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-03-15 23:29:05 -0300

    fix cookie mobile

--- src/utils/cookies.util.ts
+++ src/utils/cookies.util.ts
@@ -1,6 +1,20 @@
 import { Response, CookieOptions } from "express";
 
 const isProduction = process.env.NODE_ENV === "production";
+const frontendUrl = process.env.FRONTEND_URL;
+
+const cookieDomain = (() => {
+    if (!isProduction || !frontendUrl) {
+        return undefined;
+    }
+
+    try {
+        const hostname = new URL(frontendUrl).hostname;
+        return hostname || undefined;
+    } catch {
+        return undefined;
+    }
+})();
 
 export const COOKIE_NAMES = {
     ACCESS_TOKEN: "accessToken",
@@ -12,6 +26,7 @@ export const COOKIE_OPTIONS: CookieOptions = {
     sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
+    ...(cookieDomain ? { domain: cookieDomain } : {}),
 };
 
 export const setAccessTokenCookie = (res: Response, token: string): void => {
@@ -29,7 +44,8 @@ export const clearAccessTokenCookie = (res: Response): void => {
             httpOnly: true,
             secure: isProduction,
             sameSite: "lax",
-            path: "/"
+            path: "/",
+            ...(cookieDomain ? { domain: cookieDomain } : {}),
         }
     );
 };
